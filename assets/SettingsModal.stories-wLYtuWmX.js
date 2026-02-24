96e0ca81ac2ce44047fe0c6727a23f19081ad507
Leonezz <Leonezz@users.noreply.github.com>
2026-02-24 14:12:38 +0000

    deploy: 2fc078f9f4c95329578c523609a7c05807d2dd95

---
+++ assets/SettingsModal.stories-wLYtuWmX.js
@@ -0,0 +1,10 @@
+import{S as t}from"./SettingsModal-xaVEM6sU.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-CHNimIL-.js";import"./preload-helper-PPVm8Dsz.js";import"./Motion-DJidqmJU.js";import"./proxy-BxjV94RI.js";import"./SettingsPage-B2ea75W1.js";import"./Icon-CbxJ13Fx.js";import"./chevron-right-BIcS2Lr7.js";import"./rruleset-Bu4wjl4G.js";import"./logger-CDMKXnH4.js";import"./SettingsPageGeneral-B7iTiIDJ.js";import"./SettingsPageAdvanced-BrNjJe4B.js";import"./index-DbN_6IAz.js";import"./index-BsxoV8sq.js";import"./index-CCLpu-J3.js";import"./index-C-ek_KF4.js";const h={title:"Settings/SettingsModal",component:t,parameters:{layout:"fullscreen"},tags:["autodocs"]},a={theme:"light",dateFormat:"MM, DD",showCompleted:!0,showProgressBar:!0,soundEnabled:!0,fontSize:"base",useRelativeDates:!0,groupingStrategy:["dueAt"],aiConfig:{enabled:!0,defaultMode:!0,activeProvider:"gemini",providers:{gemini:{apiKey:"",baseUrl:"",model:""},anthropic:{apiKey:"",baseUrl:"",model:""},openai:{apiKey:"",baseUrl:"",model:""},"openai-compatible":{apiKey:"",model:"",baseUrl:""}},systemPrompt:""},voiceConfig:{enabled:!0,activeProvider:"browser",language:"",providers:{browser:{},openai:{apiKey:"",baseUrl:"https://api.openai.com/v1/audio/transcriptions",model:"whisper-1"},gemini:{apiKey:"",model:"gemini-1.5-flash"}}},defaultFocusMode:!0,totalTokenUsage:0,defaultCategory:"",filters:{tags:{include:["#"],exclude:[]},categories:{include:[],exclude:[]},priorities:{include:[],exclude:[]},statuses:{include:[],exclude:[]},enableScript:!1,script:""},sort:{field:"createdAt",direction:"asc",script:""}},e={args:{isOpen:!0,onClose:()=>{},settings:a,onUpdateSettings:()=>{},availableCategories:[],availableTags:[]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    settings,
+    onUpdateSettings: () => {},
+    availableCategories: [],
+    availableTags: []
+  }
+}`,...e.parameters?.docs?.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,h as default};
---
+++ assets/index-CCLpu-J3.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-CHNimIL-.js";import{r as o}from"./index-C-ek_KF4.js";var t=o();const m=r(t);export{m as R,t as r};
