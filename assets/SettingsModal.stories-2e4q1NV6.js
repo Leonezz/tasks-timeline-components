cc9e2d89580763ce1d25dc746a20516abbaf7579
Leonezz <Leonezz@users.noreply.github.com>
2026-06-25 14:35:53 +0000

    deploy: 511920a303283e3a5f9c78d4065e818c5ffedf61

---
+++ assets/SettingsModal.stories-2e4q1NV6.js
@@ -0,0 +1,10 @@
+import{S as t}from"./SettingsModal-DqOM_wNl.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-DnKPUOZg.js";import"./preload-helper-PPVm8Dsz.js";import"./Motion-Cm0WDyHc.js";import"./proxy-yS7xihDB.js";import"./SettingsPage-B72PVp99.js";import"./Icon-CcwwHUof.js";import"./chevron-right-OktepZ0P.js";import"./rruleset-Bu4wjl4G.js";import"./logger-CDMKXnH4.js";import"./SettingsPageGeneral-CR5SHlO8.js";import"./SettingsPageAI-CyWmP0sB.js";import"./index-W0oIwZW8.js";import"./index-DKNx8mn8.js";const y={title:"Settings/SettingsModal",component:t,parameters:{layout:"fullscreen"},tags:["autodocs"]},a={theme:"light",dateFormat:"MM, DD",showCompleted:!0,showProgressBar:!0,soundEnabled:!0,fontSize:"base",useRelativeDates:!0,groupingStrategy:["dueAt"],aiConfig:{enabled:!0,defaultMode:!0,activeProvider:"gemini",providers:{gemini:{apiKey:"",baseUrl:"",model:""},anthropic:{apiKey:"",baseUrl:"",model:""},openai:{apiKey:"",baseUrl:"",model:""},"openai-compatible":{apiKey:"",model:"",baseUrl:""}},systemPrompt:""},voiceConfig:{enabled:!0,activeProvider:"browser",language:"",providers:{browser:{},openai:{apiKey:"",baseUrl:"https://api.openai.com/v1/audio/transcriptions",model:"whisper-1"},gemini:{apiKey:"",model:"gemini-1.5-flash"}}},defaultFocusMode:!0,totalTokenUsage:0,tokenUsageByModel:{},defaultCategory:"",filters:{tags:{include:["#"],exclude:[]},categories:{include:[],exclude:[]},priorities:{include:[],exclude:[]},statuses:{include:[],exclude:[]},enableScript:!1,script:""},sort:{field:"createdAt",direction:"asc",script:""}},e={args:{isOpen:!0,onClose:()=>{},settings:a,onUpdateSettings:()=>{},availableCategories:[],availableTags:[]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    settings,
+    onUpdateSettings: () => {},
+    availableCategories: [],
+    availableTags: []
+  }
+}`,...e.parameters?.docs?.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,y as default};
---
+++ assets/index-DK6GNDnv.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-DnKPUOZg.js";import{r as o}from"./index-DWoQQhJd.js";var t=o();const m=r(t);export{m as R,t as r};
